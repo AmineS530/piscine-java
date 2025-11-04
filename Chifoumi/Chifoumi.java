e8220bc4dca8570dfc5eabe88bc8953c9c01822b
Sadik Amine <idekwiam18@gmail.com>
2025-11-04 14:44:23 +0100

    switch exp, *sips tea*

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,16 @@
+
+public class Chifoumi {
+
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        return switch (chifoumiAction) {
+            case ROCK ->
+                ChifoumiAction.SCISSOR;
+            case PAPER ->
+                ChifoumiAction.ROCK;
+            case SCISSOR ->
+                ChifoumiAction.PAPER;
+            default ->
+                null;
+        };
+    }
+}
