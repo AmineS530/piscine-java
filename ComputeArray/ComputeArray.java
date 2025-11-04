1ab797c9bd5ab9897a8dec02af2f296148f8af07
Sadik Amine <idekwiam18@gmail.com>
2025-11-04 18:43:38 +0100

    compute this!

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,19 @@
+
+public class ComputeArray {
+
+    public static int[] computeArray(int[] array) {
+        int n = array.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = switch (array[i] % 3) {
+                case 0 ->
+                    array[i] * 5;
+                case 1 ->
+                    array[i] + 7;
+                default ->
+                    array[i];
+            };
+        }
+        return result;
+    }
+}
