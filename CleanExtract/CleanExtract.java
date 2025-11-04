da447dab61ece6149c25d948b38573c85f1cd008
Sadik Amine <idekwiam18@gmail.com>
2025-11-04 18:25:10 +0100

    workoing clean extract hopefully

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -9,21 +9,21 @@ public class CleanExtract {
             phrase = phrase.trim();
             int first_dot = phrase.indexOf('.');
             int last_dot = phrase.lastIndexOf('.');
-            if (first_dot != -1 && last_dot != -1) {
+            if (first_dot != -1) {
                 String curr;
                 if (first_dot != last_dot) {
                     curr = phrase.substring(first_dot + 1, last_dot).trim();
                 } else {
                     curr = phrase.substring(first_dot + 1).trim();
+                }
+                if (!curr.isEmpty()) {
                     if (res.length() > 0) {
                         res.append(" ");
                     }
-                }
-                if (curr.isEmpty()) {
-                    res.append(" ");
-                } else {
                     res.append(curr);
                 }
+            } else {
+                res.append(phrase);
             }
         }
         return res.toString();
