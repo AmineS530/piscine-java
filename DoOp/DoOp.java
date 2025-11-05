5ec517ebbbd449405486b5733cff87276ed94c55
Sadik Amine <idekwiam18@gmail.com>
2025-11-05 21:41:33 +0100

    another test push

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -3,7 +3,7 @@ public class DoOp {
 
     public static String operate(String[] args) {
         if (args.length != 3) {
-            return "it depend on the input.";
+            return "Error";
         }
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[2]);
