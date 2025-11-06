4b1da0b2dbcad24bc43b8ec1cd5f7a759d44ecf9
Sadik Amine <idekwiam18@gmail.com>
2025-11-06 03:06:04 +0100

    capitalize

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+
+public class Capitalize {
+
+    public static void capitalize(String[] args) throws IOException {
+        if (args.length != 2) {
+            return;
+        }
+        String inputFile = args[0];
+        String outputFile = args[1];
+
+        try (FileInputStream fis = new FileInputStream(inputFile); FileOutputStream fos = new FileOutputStream(outputFile)) {
+            String input = new String(fis.readAllBytes()).toLowerCase().trim();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+
+                if (i == 0 || input.charAt(i - 1) == ' ') {
+                    if (c >= 'a' && c <= 'z') {
+                        sb.append(Character.toUpperCase(c));
+                    }
+                } else {
+                    sb.append(c);
+                }
+            }
+            fos.write(sb.toString().getBytes());
+        }
+    }
+
+}
