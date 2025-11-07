6cc853be021ecf4747ebc547c95e25d52fb66512
Amine Sadik <idekwiam18@gmail.com>
2025-11-07 14:11:03 +0100

    StarConstructorsclear

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,41 @@
+
+public class CelestialObject {
+
+    public double x;
+    public double z;
+    public double y;
+    public String name;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setX(double newx) {
+        x = newx;
+    }
+
+    public void setY(double newy) {
+        y = newy;
+    }
+
+    public void setZ(double newz) {
+        z = newz;
+    }
+
+    public void setName(String newname) {
+        name = newname;
+    }
+
+}
