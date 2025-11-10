31d30d609144e160ed9d29375946764d521cf93e
Sadik Amine <idekwiam18@gmail.com>
2025-11-10 18:26:05 +0100

    forgot the dot at the end

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -24,9 +24,9 @@ public class Sorcerer extends Character implements Healer {
 
     @Override
     public String toString() {
-        return this.getCurrentHealth() == 0 ? this.getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP"
+        return this.getCurrentHealth() == 0 ? this.getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP."
                 : this.getName() + " is a sorcerer with " + this.getCurrentHealth() + " HP. It can heal "
-                + this.healCapacity + " HP";
+                + this.healCapacity + " HP.";
     }
 
 }
