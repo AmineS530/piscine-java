d55553b8dc2eb55f173f2c56d6948968d7071de9
Sadik Amine <idekwiam18@gmail.com>
2025-11-21 15:59:01 +0100

    Excalibur

---
+++ Singleton/Excalibur.java
@@ -0,0 +1,18 @@
+
+public class Excalibur {
+
+    private final String name;
+    private static final Excalibur INSTANCE = new Excalibur("Excalibur");
+
+    private Excalibur(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Excalibur getInstance() {
+        return INSTANCE;
+    }
+}
