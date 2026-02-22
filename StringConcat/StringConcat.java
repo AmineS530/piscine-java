55ec4c22d41488a3f3842c6d9c1a488dceb3f7af
Amine Sadik <idekwiam18@gmail.com>
2026-02-22 12:44:09 +0000

    concat fixed

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,13 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        return s1 + s2;
+        if (s1 != null && s2 == null) {
+            return s1;
+        } else if (s1 == null && s2 != null) {
+            return s2;
+        } else if (s1 == null && s2 == null) {
+            return null;
+        } else {
+            return s1 + s2;
+        }
     }
 }
