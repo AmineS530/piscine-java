94557371a00386beadb0db441aa01b475a78ff44
Amine Sadik <idekwiam18@gmail.com>
2026-03-06 22:38:46 +0000

    StreamMap

---
+++ StreamMap/StreamMap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMap {
+    public static Integer sumOfStringLength(Stream<String> s) {
+        return s.mapToInt(String::length).sum();
+    }
+
+    public static List<String> upperCaseAllString(Stream<String> s) {
+        return s.map(String::toUpperCase).collect(Collectors.toList());
+    }
+    
+    public static Set<Integer> uniqIntValuesGreaterThan42(Stream<Double> s) {
+        return s.map(Double::intValue).filter(i -> i > 42).collect(Collectors.toSet());
+    }
+}
