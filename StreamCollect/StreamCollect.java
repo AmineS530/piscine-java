29fff1746dd515c051f5adc7776f9cfbf4858329
Amine Sadik <idekwiam18@gmail.com>
2026-03-06 22:25:13 +0000

    StreamCollect

---
+++ StreamCollect/StreamCollect.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+public class StreamCollect {
+    public static Map<Character, List<String>> mapByFirstLetter(Stream<String> s) {
+        return s.collect(Collectors.groupingBy(str -> Character.toUpperCase(str.charAt(0))));
+    }
+
+    public static Map<Integer, Optional<Integer>> getMaxByModulo4(Stream<Integer> s) {
+        return s.collect(Collectors.groupingBy(i -> i % 4, Collectors.maxBy(Integer::compareTo)));
+    }
+
+    public static String orderAndConcatWithSharp(Stream<String> s) {
+        return "##{" + s.sorted().collect(Collectors.joining(" # ")) + "}";
+    }
+}
