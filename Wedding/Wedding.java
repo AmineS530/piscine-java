e7dc8cccc96e7d365b8b7193bd9c1fb23bd9eeae
Amine Sadik <idekwiam18@gmail.com>
2026-03-07 00:13:17 +0000

    Wedding

---
+++ Wedding/Wedding.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Wedding {
+    public static Map<String, String> createCouple(Set<String> f, Set<String> s) {
+        List<String> first = new ArrayList<>(f);
+        List<String> second = new ArrayList<>(s);
+        return IntStream.range(0, Math.min(first.toArray().length, second.toArray().length))
+                .boxed()
+                .collect(Collectors.toMap(first.stream().toList()::get, second.stream().toList()::get));
+    }
+
+}
