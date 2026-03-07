b06b9140aed66b96e9b748849d9a2c338e9bee12
Amine Sadik <idekwiam18@gmail.com>
2026-03-07 00:14:15 +0000

    Wedding opt

--- Wedding/Wedding.java
+++ Wedding/Wedding.java
@@ -6,7 +6,7 @@ public class Wedding {
     public static Map<String, String> createCouple(Set<String> f, Set<String> s) {
         List<String> first = new ArrayList<>(f);
         List<String> second = new ArrayList<>(s);
-        return IntStream.range(0, Math.min(first.toArray().length, second.toArray().length))
+        return IntStream.range(0, Math.min(first.size(), second.size()))
                 .boxed()
                 .collect(Collectors.toMap(first::get, second::get));
     }
