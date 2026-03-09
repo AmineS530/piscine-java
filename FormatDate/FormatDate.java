efa5825b0b6bf5e69f366bd55b44334ba6302cc2
Sadik Amine <idekwiam18@gmail.com>
2026-03-09 15:54:26 +0000

    FormatDate

---
+++ FormatDate/FormatDate.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class FormatDate {
+
+    public static String formatToFullText(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("'Le' d MMMM 'de l''an' yyyy 'à' HH'h'mm'm et' ss's'", Locale.FRANCE);
+        return dateTime.format(formatter);
+    }
+
+    public static String formatSimple(LocalDate date) {
+        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd YY");
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern("MMMM dd yy", Locale.ITALIAN);
+        return date.format(formatter);
+    }
+
+    public static String formatIso(LocalTime time) {
+        return time.format(DateTimeFormatter.ISO_LOCAL_TIME);
+    }
+
+}
